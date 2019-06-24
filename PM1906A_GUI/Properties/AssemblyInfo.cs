01e2537e63734e57d223fbd36e11de3551683221
Mikk.Su <mikk.su@irixi.com>
2019-06-24 13:34:33 +0800

    Change the version of the GUI to 1.0.1.21. Change the version of the Helper to 1.0.1.22.

--- PM1906A_GUI/Properties/AssemblyInfo.cs
+++ PM1906A_GUI/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.21")]
+[assembly: AssemblyFileVersion("1.0.1.21")]
 [assembly: Guid("AD2E5E5F-1489-48E5-AC5B-23C2C7851173")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
